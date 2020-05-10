d0fa7708cec0a73f374303ed9550ac2bbea3920b
ExciteTaco <ericklima34@gmail.com>
2020-05-09 20:47:45 -0500

    Add comments and todos in SearchBar.js

--- spotify-playlist/src/Components/SearchBar/SearchBar.js
+++ spotify-playlist/src/Components/SearchBar/SearchBar.js
@@ -2,18 +2,25 @@ import React from "react";
 import "./SearchBar.css";
 
 export class SearchBar extends React.Component {
+  // sets the components state
   state = {
     term: "",
   };
+  // Passes the compnents state to app.js
   search = () => {
     this.props.onSearch(this.state.term);
   };
+  // accepts the event from the onClick button attribute
   handleTermChange = (event) => {
+    // Obtain the value from the event and sets the state
     this.setState({
       term: event.target.value,
     });
   };
+
   render() {
+    // ToDo
+    // Make form for when user presses enter it will submit for search
     return (
       <div className="SearchBar">
         <input
