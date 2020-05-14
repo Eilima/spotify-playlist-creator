df18e1b2b317987cbf5fc412a5957681643730cd
ExciteTaco <ericklima34@gmail.com>
2020-05-13 21:24:15 -0500

    Add some comments describing code in Spotify.js

--- spotify-playlist/src/Components/util/Spotify.js
+++ spotify-playlist/src/Components/util/Spotify.js
@@ -1,23 +1,32 @@
-const clientId = ""; // Insert client ID here.
+const clientId = ""; // Insert client ID(Spotify APIs) here.
 const redirectUri = "http://localhost:3000/"; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
 let accessToken;
 
 const Spotify = {
+  // Will return accessToken if it exist already or get a new one
   getAccessToken() {
+    // Checks if access token exist
     if (accessToken) {
       return accessToken;
     }
 
+    // Looks in the window url to find accessToken
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    // Looks in the window url to find expires in token
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    // Checks if both exist in url
     if (accessTokenMatch && expiresInMatch) {
+      // Stores accessToken from whats found in the url
       accessToken = accessTokenMatch[1];
+      // Converts url expiresIn token to number and stores it
       const expiresIn = Number(expiresInMatch[1]);
+
       window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
       window.history.pushState("Access Token", null, "/"); // This clears the parameters, allowing us to grab a new access token when it expires.
       return accessToken;
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+      // Sets window url equal to accessUrl
       window.location = accessUrl;
     }
   },
