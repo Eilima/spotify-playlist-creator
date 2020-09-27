fc10b9882851eca974c14853cad7c205d0dcbd56
Eilima <ericklima34@gmail.com>
2020-09-26 23:17:56 -0500

    Update TrackList component to functional component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./TrackList.css";
 import { Track } from "../Track/Track";
 
-export class TrackList extends React.Component {
-  render() {
-    return (
-      <div className="TrackList">
-        {this.props.tracks.map((track) => {
-          return (
-            <Track
-              track={track}
-              key={track.id}
-              onAdd={this.props.onAdd}
-              onRemove={this.props.onRemove}
-              isRemoval={this.props.isRemoval}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+export const TrackList = ({ results, onRemove, isRemoval, onAdd }) => {
+  return (
+    <div className="TrackList">
+      {results
+        ? results.map((track) => {
+            return (
+              <Track
+                track={track}
+                key={track.id}
+                onAdd={onAdd}
+                onRemove={onRemove}
+                isRemoval={isRemoval}
+              />
+            );
+          })
+        : null}
+    </div>
+  );
+};
