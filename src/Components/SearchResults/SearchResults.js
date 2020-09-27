0fd5f943dcf9ec51f3a347bbea5cc2a13269d642
Eilima <ericklima34@gmail.com>
2020-09-26 23:16:51 -0500

    Update SearchResults component to function component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,17 +2,11 @@ import React from "react";
 import "./SearchResults.css";
 import { TrackList } from "../TrackList/TrackList.js";
 
-export class SearchResults extends React.Component {
-  render() {
-    return (
-      <div className="SearchResults">
-        <h2>Results</h2>
-        <TrackList
-          tracks={this.props.searchResults}
-          onAdd={this.props.onAdd}
-          isRemoval={false}
-        />
-      </div>
-    );
-  }
-}
+export const SearchResults = ({ results, onAdd }) => {
+  return (
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList results={results} onAdd={onAdd} isRemoval={false} />
+    </div>
+  );
+};
