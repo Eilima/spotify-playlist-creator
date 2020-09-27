6c250bd7e211dcb3a762ec5508645bbb3fc35372
Eilima <ericklima34@gmail.com>
2020-09-26 23:14:26 -0500

    Update Playlist component to functional component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,23 +2,27 @@ import React from "react";
 import "./Playlist.css";
 import { TrackList } from "../TrackList/TrackList";
 
-export class Playlist extends React.Component {
-  handleNameChange = (event) => {
-    this.props.onNameChange(event.target.value);
+export const Playlist = ({
+  onNameChange,
+  playlistTracks,
+  playlistName,
+  onRemove,
+  onSave,
+}) => {
+  const handleNameChange = (event) => {
+    onNameChange(event.target.value);
   };
-  render() {
-    return (
-      <div className="Playlist">
-        <input placeholder={"New Playlist"} onChange={this.handleNameChange} />
-        <TrackList
-          tracks={this.props.playlistTracks}
-          onRemove={this.props.onRemove}
-          isRemoval={true}
-        />
-        <button className="Playlist-save" onClick={this.props.onSave}>
-          SAVE TO SPOTIFY
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Playlist">
+      <input placeholder={"New Playlist"} onChange={handleNameChange} />
+      <TrackList
+        results={playlistTracks}
+        onRemove={onRemove}
+        isRemoval={true}
+      />
+      <button className="Playlist-save" onClick={onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  );
+};
