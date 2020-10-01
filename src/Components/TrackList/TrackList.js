e11ae4803f9231608fd870f2dc5489fd8d7fafbc
Eilima <ericklima34@gmail.com>
2020-09-30 22:14:37 -0500

    Add comments to the TrackList component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,19 +5,25 @@ import { Track } from "../Track/Track";
 export const TrackList = ({ results, onRemove, isRemoval, onAdd }) => {
   return (
     <div className="TrackList">
-      {results
-        ? results.map((track) => {
-            return (
-              <Track
-                track={track}
-                key={track.id}
-                onAdd={onAdd}
-                onRemove={onRemove}
-                isRemoval={isRemoval}
-              />
-            );
-          })
-        : null}
+      {/* Uses a ternary operator to determine wether to display Track component */}
+      {
+        // if results !== null or results === null
+        results
+          ? // if true, results !== null, map through array returning a Track component for each element in the array
+            results.map((track) => {
+              return (
+                <Track
+                  track={track}
+                  key={track.id}
+                  onAdd={onAdd}
+                  onRemove={onRemove}
+                  isRemoval={isRemoval}
+                />
+              );
+            })
+          : // else, results === null, return null (display nothing).
+            null
+      }
     </div>
   );
 };
